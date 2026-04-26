5329ec3d889c36acbdf6b9e5ffc93e01f1548b78
Stan <stanislaw.szenk@gmail.com>
2026-04-26 15:10:23 +0200

    refactor(routes): group api v1 routes by domain

--- routes/api.php
+++ routes/api.php
@@ -12,34 +12,87 @@ Route::prefix('v1')
     ->as('api.v1.')
     ->group(function () {
         Route::middleware('throttle:60,1')->group(function () {
-            Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
-            Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
-            Route::get('/auth/discord/redirect', [DiscordAuthController::class, 'redirect'])->name('auth.discord.redirect');
-            Route::get('/auth/discord/callback', [DiscordAuthController::class, 'callback'])->name('auth.discord.callback');
+            Route::controller(AuthController::class)
+                ->name('auth.')
+                ->group(function () {
+                    Route::post('/register', 'register')->name('register');
+                    Route::post('/login', 'login')->name('login');
+                });
+
+            Route::prefix('auth/discord')
+                ->controller(DiscordAuthController::class)
+                ->name('auth.discord.')
+                ->group(function () {
+                    Route::get('/redirect', 'redirect')->name('redirect');
+                    Route::get('/callback', 'callback')->name('callback');
+                });
         });
 
         Route::middleware(['auth:sanctum', 'throttle:60,1'])->group(function () {
-            Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
-            Route::get('/me', [AuthController::class, 'me'])->name('auth.me');
-            Route::post('/refresh', [AuthController::class, 'refresh'])->name('auth.refresh');
-            Route::get('/tenants', [TenantController::class, 'index'])->name('tenants.index');
-            Route::post('/tenants', [TenantController::class, 'store'])->name('tenants.store');
-            Route::post('/tenants/{tenant}/switch', [TenantController::class, 'switchCurrent'])->name('tenants.switch');
-            Route::get('/tenants/current/members', [TenantMemberController::class, 'index'])->name('tenants.members.index');
-            Route::post('/tenants/current/members', [TenantMemberController::class, 'store'])->name('tenants.members.store');
-            Route::patch('/tenants/current/members/{user}', [TenantMemberController::class, 'update'])->name('tenants.members.update');
-            Route::delete('/tenants/current/members/{user}', [TenantMemberController::class, 'destroy'])->name('tenants.members.destroy');
-            Route::get('/tenants/current/invitations', [TenantInvitationController::class, 'index'])->name('tenants.invitations.index');
-            Route::post('/tenants/current/invitations', [TenantInvitationController::class, 'store'])->name('tenants.invitations.store');
-            Route::post('/tenants/current/invitations/{tenantInvitation}/resend', [TenantInvitationController::class, 'resend'])->name('tenants.invitations.resend');
-            Route::post('/tenants/invitations/{token}/accept', [TenantInvitationController::class, 'accept'])->name('tenants.invitations.accept');
-            Route::delete('/tenants/current/invitations/{tenantInvitation}', [TenantInvitationController::class, 'destroy'])->name('tenants.invitations.destroy');
-            Route::get('/discord/integration', [DiscordIntegrationController::class, 'show'])->name('discord.integration.show');
-            Route::put('/discord/integration', [DiscordIntegrationController::class, 'upsert'])->name('discord.integration.upsert');
-            Route::patch('/discord/integration/credentials', [DiscordIntegrationController::class, 'updateCredentials'])->name('discord.integration.credentials.update');
-            Route::delete('/discord/integration/credentials', [DiscordIntegrationController::class, 'clearCredentials'])->name('discord.integration.credentials.clear');
-            Route::get('/discord/guild-roles', [DiscordIntegrationController::class, 'listGuildRoles'])->name('discord.guild-roles.index');
-            Route::post('/discord/role-mappings', [DiscordIntegrationController::class, 'storeRoleMapping'])->name('discord.role-mappings.store');
-            Route::delete('/discord/role-mappings/{mapping}', [DiscordIntegrationController::class, 'destroyRoleMapping'])->name('discord.role-mappings.destroy');
+            Route::controller(AuthController::class)
+                ->name('auth.')
+                ->group(function () {
+                    Route::post('/logout', 'logout')->name('logout');
+                    Route::get('/me', 'me')->name('me');
+                    Route::post('/refresh', 'refresh')->name('refresh');
+                });
+
+            Route::prefix('tenants')
+                ->name('tenants.')
+                ->group(function () {
+                    Route::controller(TenantController::class)->group(function () {
+                        Route::get('/', 'index')->name('index');
+                        Route::post('/', 'store')->name('store');
+                        Route::post('/{tenant}/switch', 'switchCurrent')->name('switch');
+                    });
+
+                    Route::prefix('current')->group(function () {
+                        Route::prefix('members')
+                            ->controller(TenantMemberController::class)
+                            ->name('members.')
+                            ->group(function () {
+                                Route::get('/', 'index')->name('index');
+                                Route::post('/', 'store')->name('store');
+                                Route::patch('/{user}', 'update')->name('update');
+                                Route::delete('/{user}', 'destroy')->name('destroy');
+                            });
+
+                        Route::prefix('invitations')
+                            ->controller(TenantInvitationController::class)
+                            ->name('invitations.')
+                            ->group(function () {
+                                Route::get('/', 'index')->name('index');
+                                Route::post('/', 'store')->name('store');
+                                Route::post('/{tenantInvitation}/resend', 'resend')->name('resend');
+                                Route::delete('/{tenantInvitation}', 'destroy')->name('destroy');
+                            });
+                    });
+
+                    Route::post('/invitations/{token}/accept', [TenantInvitationController::class, 'accept'])
+                        ->name('invitations.accept');
+                });
+
+            Route::prefix('discord')
+                ->name('discord.')
+                ->controller(DiscordIntegrationController::class)
+                ->group(function () {
+                    Route::prefix('integration')
+                        ->name('integration.')
+                        ->group(function () {
+                            Route::get('/', 'show')->name('show');
+                            Route::put('/', 'upsert')->name('upsert');
+                            Route::patch('/credentials', 'updateCredentials')->name('credentials.update');
+                            Route::delete('/credentials', 'clearCredentials')->name('credentials.clear');
+                        });
+
+                    Route::get('/guild-roles', 'listGuildRoles')->name('guild-roles.index');
+
+                    Route::prefix('role-mappings')
+                        ->name('role-mappings.')
+                        ->group(function () {
+                            Route::post('/', 'storeRoleMapping')->name('store');
+                            Route::delete('/{mapping}', 'destroyRoleMapping')->name('destroy');
+                        });
+                });
         });
     });
